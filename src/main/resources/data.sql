6627aa4db36c9b62c560bcd3899d68230d0f50fb
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-10-14 09:20:35 +0900

    init commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO course(id, name) VALUES(10001, 'JPA practice');
+INSERT INTO course(id, name) VALUES(10002, 'Spring practice');
+INSERT INTO course(id, name) VALUES(10003, 'Spring Boot practice');
+
+-- SELECT * FROM COURSE WHERE name LIKE '%Updated'
---
+++ src/test/java/com/kiyotagbangers/jpahibernatedemo/JpaHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyotagbangers.jpahibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
