f527e055f310c97af1c087245870b25cabc6c661
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-10-18 15:11:13 +0900

    Inheritance strategy JOINED

--- src/main/java/com/kiyotagbangers/jpahibernatedemo/entity/Employee.java
+++ src/main/java/com/kiyotagbangers/jpahibernatedemo/entity/Employee.java
@@ -4,8 +4,9 @@ import javax.persistence.*;
 
 @Entity
 // @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Default strategy.All subclasses are mapped to one table
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)  // Individual table will be created for per concrete entity class
-@DiscriminatorColumn(name = "EmployeeType")
+// @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)  // Individual table will be created for per concrete entity class
+@Inheritance(strategy = InheritanceType.JOINED)
+// @DiscriminatorColumn(name = "EmployeeType")
 public abstract class Employee {
 
     @Id
