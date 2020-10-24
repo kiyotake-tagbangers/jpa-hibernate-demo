d46f3eb51b0ef876254bc180fc305f2d6e1e3d43
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-10-24 10:43:24 +0900

    add course repository test

--- src/main/java/com/kiyotagbangers/jpahibernatedemo/repository/CourseSpringDataRepository.java
+++ src/main/java/com/kiyotagbangers/jpahibernatedemo/repository/CourseSpringDataRepository.java
@@ -5,21 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 // @Repository
 @RepositoryRestResource(path = "courses")
+@Transactional
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
+
     List<Course> findByNameAndId(String name, Long id);
 
     List<Course> findByName(String name);
 
-    List<Course> countByName(String name);
-
     List<Course> findByNameOrderByIdDesc(String name);
 
-    List<Course> deleteByName(String name);
+    void deleteByName(String name);
 
     @Query("Select c From Course c where name like '% practice'")
     List<Course> courseWithPracticeInName();
--- src/test/java/com/kiyotagbangers/jpahibernatedemo/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/com/kiyotagbangers/jpahibernatedemo/repository/CourseSpringDataRepositoryTest.java
@@ -2,6 +2,7 @@ package com.kiyotagbangers.jpahibernatedemo.repository;
 
 import com.kiyotagbangers.jpahibernatedemo.JpaHibernateDemoApplication;
 import com.kiyotagbangers.jpahibernatedemo.entity.Course;
+import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = JpaHibernateDemoApplication.class)
 class CourseSpringDataRepositoryTest {
@@ -24,15 +25,15 @@ class CourseSpringDataRepositoryTest {
 
     @Autowired
     CourseSpringDataRepository repository;
-    
+
     @Test
-    public void findById_CoursePresent(){
+    public void findById_CoursePresent() {
         Optional<Course> courseOptional = repository.findById(10001L);
         assertTrue(courseOptional.isPresent());
     }
 
     @Test
-    public void findById_CourseNotPresent(){
+    public void findById_CourseNotPresent() {
         Optional<Course> courseOptional = repository.findById(20001L);
         assertFalse(courseOptional.isPresent());
     }
@@ -108,6 +109,30 @@ class CourseSpringDataRepositoryTest {
     }
 
     @Test
+    void findUsingNameAndIdSucceed() {
+        List<Course> courses = repository.findByNameAndId("JPA exercise", 10001L);
+        assertThat(courses).extracting("name").contains("JPA exercise");
+    }
+
+    @Test
+    void findUsingNameAndIdFailure() {
+        List<Course> courses = repository.findByNameAndId("JPA exercise", 10002L);
+        assertThat(courses).hasSize(0);
+        //     select
+        //        course0_.id as id1_0_,
+        //        course0_.created_date as created_2_0_,
+        //        course0_.last_updated_date as last_upd3_0_,
+        //        course0_.name as name4_0_
+        //    from
+        //        course course0_
+        //    where
+        //        course0_.name=?
+        //        and course0_.id=?
+        //2020-10-23 08:00:03.925 TRACE 1584 --- [           main] o.h.type.descriptor.sql.BasicBinder      : binding parameter [1] as [VARCHAR] - [JPA exercise]
+        //2020-10-23 08:00:03.925 TRACE 1584 --- [           main] o.h.type.descriptor.sql.BasicBinder      : binding parameter [2] as [BIGINT] - [10002]
+    }
+
+    @Test
     void findUsingName() {
         logger.info("FindByName -> {}", repository.findByName("JPA exercise"));
         //     select
@@ -121,5 +146,37 @@ class CourseSpringDataRepositoryTest {
         //        course0_.name=?
         //2020-10-22 08:41:11.104 TRACE 14321 --- [           main] o.h.type.descriptor.sql.BasicBinder      : binding parameter [1] as [VARCHAR] - [JPA exercise]
         //  FindByName -> [Course{name='JPA exercise'}]
+    }
+
+    @Test
+    void deleteByName() {
+        repository.deleteByName("Spring practice");
+        List<Course> courses = repository.findAll();
+        assertThat(courses).extracting("name").doesNotContain("Spring practice");
+    }
+
+    @Test
+    void courseStudentsLazyInitializeException() {
+        Optional<Course> courseOptional = repository.findById(10001L);
+        logger.info("Course name: {}", courseOptional.get().getName());
+        assertThrows(LazyInitializationException.class, () -> {
+            courseOptional.get().getReviews().get(0);
+        });
+
+        // If Course class reviews field is EAGER fetch
+        // logger.info("Review: {}", courseOptional.get().getReviews().get(0));
+    }
+
+    @Test
+    void findByNameOrderByIdDesc() {
+
+        Course course = new Course("Spring Boot practice");
+        repository.save(course);
+
+        List<Course> courses = repository.findByNameOrderByIdDesc("Spring Boot practice");
+        Long firstElementId = courses.get(0).getId();
+        Long secondElementId = courses.get(1).getId();
+
+        assertThat(firstElementId).isGreaterThan(secondElementId);
     }
 }
